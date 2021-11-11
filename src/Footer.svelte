93fa59cddaad383ea68e209299b3720f95bd01f5
Edward0120 <edwardrubengelegino@gmail.com>
2021-11-11 18:37:08 +0800

    Revert "Footer add"
    This reverts commit 468765177daf0e394942f428e2f8e3ab067544a2.

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -5,7 +5,7 @@
 <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        <strong>This website</strong> by <a href="https://github.com/JeremiasBulanadi">Jeremias Bulanadi & Edward Elegino</a> was made with the use of
+        <strong>This website</strong> by <a href="https://github.com/JeremiasBulanadi">Jeremias Bulanadi</a> was made with the use of
         <a href="https://svelte.dev/">Svelte</a> and <a href="https://bulma.io/">Bulma</a>.<br/>The website was done in partial fulfillment of the requirements in Application and Systems Integration.
       </p>
     </div>
